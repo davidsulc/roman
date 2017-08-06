92073d1b856e5adab09b7317f0ca0e20e91e74ce
David Sulc <davidsulc@gmail.com>
2017-08-06 18:10:23 +0200

    added docs for

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,6 +1,6 @@
 defmodule Roman do
   @moduledoc """
-  Functions to work with roman numerals
+  Functions to work with roman numerals from 1 to 4000 (not included)
   """
 
   @external_resource "lib/numerals.txt"
@@ -16,8 +16,45 @@ defmodule Roman do
   @type error :: {atom, atom, String.t}
   @type numeral :: String.t
 
+  @doc """
+  Returns a list of `{int_value, numeral_string}` tuples for all possible
+  numerals.
+  """
   @spec numeral_pairs() :: [{number, numeral}]
   def numeral_pairs, do: @numeral_pairs
 
+  @doc """
+  Decodes a roman numeral string into the corresponding integer value.
+
+  This function returns:
+
+  - `{:ok, value}` - the integer value of the provided numeral.
+  - `{:error, reason, message}` - the provided numeral is invalid.
+
+  Possible errors are:
+
+  - `{:error, :empty_string, _}`: string is empty
+  - `{:error, :invalid_letter, _}`: if the provided string contains a character
+      that isn't one of I, V, X, L, C, D, M
+  - `{:error, :repeated_vld, _}`: string contains more than one instance each
+      of letters V, L, and D (i.e. numerals corresponding to numbers starting
+      with a 5)
+  - `{:error, :identical_letter_seq_too_long, _}`: string has a sequence of 4
+      or more identical letters
+  - `{:error, :sequence_increasing, _}`: string contains a value greater than
+      one appearing before it (rule applies to combined value in subtractive
+      case)
+  - `{:error, :value_greater_than_subtraction, _}`: string contains a value
+      matching or exceding a previously subtracted value
+
+  ### Examples
+
+      iex> Roman.decode("MMMDCCCXCVIII")
+      {:ok, 3898}
+      iex> Roman.decode("LLVIV")
+      {:error, :repeated_vld,
+      "letters V, L, and D can appear only once, but found several instances of L, V"}
+  """
+  @spec decode(String.t) :: number | Roman.error
   defdelegate decode(numeral), to: __MODULE__.Decoder
 end
--- mix.exs
+++ mix.exs
@@ -33,6 +33,6 @@ defmodule Roman.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
   end
 end
