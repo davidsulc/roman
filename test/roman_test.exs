c9f2dfd40fc00858f661857da1514081b8ed7cec
David Sulc <davidsulc@gmail.com>
2017-08-07 19:39:45 +0200

    moved/added tests

--- test/roman_test.exs
+++ test/roman_test.exs
@@ -1,4 +1,61 @@
 defmodule RomanTest do
   use ExUnit.Case, async: true
   doctest Roman
+
+  alias Test.Support.Converter
+
+  describe "decode/1 validations" do
+    test "empty string is invalid" do
+      assert {:error, :empty_string, _} = Roman.decode("")
+    end
+
+    test "IT is invalid" do
+      assert {:error, :invalid_letter, _} = Roman.decode("IT")
+    end
+
+    test "VIV is invalid" do
+      assert {:error, :repeated_vld, _} = Roman.decode("VIV")
+    end
+
+    test "IIII is invalid" do
+      assert {:error, :identical_letter_seq_too_long, _} =
+          Roman.decode("IIII")
+    end
+
+    test "VX is invalid" do
+      assert {:error, :sequence_increasing, _} = Roman.decode("VX")
+    end
+
+    test "CMC is invalid" do
+      assert {:error, :value_greater_than_subtraction, _} =
+          Roman.decode("CMC")
+    end
+  end
+
+  describe "decode/1" do
+    test "all valid numerals" do
+      for num <- Converter.all_numerals do
+        expected = Converter.convert(num)
+        {:ok, result} = Roman.decode(num)
+        assert result == expected,
+            "Expected decode(\"#{num}\") to yield #{expected}, but got #{result}"
+      end
+    end
+
+    test ":ignore_case option" do
+      assert {:error, _, _} = Roman.decode("x")
+      assert Roman.decode("x", ignore_case: true) == {:ok, 10}
+    end
+  end
+
+  test "decode!/1 raises ArgumentError on invalid input" do
+    assert_raise ArgumentError, fn ->
+      Roman.decode!("VV")
+    end
+  end
+
+  test "numeral?/1" do
+    assert Roman.numeral?("XVI") == true
+    assert Roman.numeral?("FOO") == false
+  end
 end
